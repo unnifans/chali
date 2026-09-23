8e922bd468591061343d9979c946d324fdff0b29
unnifans <wastedev2005@gmail.com>
2026-09-23 11:52:43 +0530

    add /mod admin path as alternate to /admin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -53,7 +53,7 @@ export default defineConfig({
       workbox: {
         // Since you have multiple pages, we need to handle navigation properly
         navigateFallback: './index.html',
-        navigateFallbackDenylist: [/^\/_/, /^\/admin/, /^\/admin\.html$/], // Exclude admin page from fallback
+        navigateFallbackDenylist: [/^\/_/, /^\/admin/, /^\/admin\.html$/, /^\/mod/, /^\/mod\.html$/], // Exclude admin page from fallback
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
